2fc25db5ddf17f602076d6bbce80940a11b1c0a4
Festive <27864684+mash-reza@users.noreply.github.com>
2019-09-16 16:53:21 +0430

    mix page video loading and controller

--- app/src/main/java/com/example/dubsmashmixer/activity/Mix.java
+++ app/src/main/java/com/example/dubsmashmixer/activity/Mix.java
@@ -4,17 +4,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
 import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
 import com.example.dubsmashmixer.R;
 import com.example.dubsmashmixer.util.Constants;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.jaygoo.widget.OnRangeChangedListener;
+import com.jaygoo.widget.RangeSeekBar;
+
+import java.io.File;
+import java.text.DecimalFormat;
 
 public class Mix extends AppCompatActivity {
     //log tag
@@ -25,22 +35,29 @@ public class Mix extends AppCompatActivity {
     VideoView mixVideoView;
     ImageButton mixVideoPlayImageButton;
     ImageButton mixVideoStopImageButton;
+    ImageButton mixVideoPauseImageButton;
     TextView mixVideoRangeStartTextView;
     TextView mixVideoRangeFinishTextView;
     CrystalRangeSeekbar mixVideoRangeSeekBar;
+    SeekBar mixVideoSeekBar;
     FloatingActionButton loadVideoFab;
 
     //audio card
     TextView audioFileNameTextView;
     ImageButton mixAudioPlayImageButton;
     ImageButton mixAudioStopImageButton;
+    ImageButton mixAudioPauseImageButton;
     TextView mixAudioRangeStartTextView;
     TextView mixAudioRangeFinishTextView;
     CrystalRangeSeekbar mixAudioRangeSeekBar;
+    SeekBar mixAudioSeekBar;
     FloatingActionButton loadAudioFab;
 
     //start button
     FloatingActionButton mixStartFab;
+
+    //handler for updating seek bar
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,18 +72,22 @@ public class Mix extends AppCompatActivity {
         mixVideoView = findViewById(R.id.mix_videoView);
         mixVideoPlayImageButton = findViewById(R.id.mix_video_play_imageButton);
         mixVideoStopImageButton = findViewById(R.id.mix_video_stop_imageButton);
+        mixVideoPauseImageButton = findViewById(R.id.mix_video_pause_imageButton);
         mixVideoRangeStartTextView = findViewById(R.id.mix_video_range_start_textView);
         mixVideoRangeFinishTextView = findViewById(R.id.mix_video_range_finish_textView);
         mixVideoRangeSeekBar = findViewById(R.id.mix_video_range_seekBar);
+        mixVideoSeekBar = findViewById(R.id.mix_video_seekBar);
         loadVideoFab = findViewById(R.id.load_video_fab);
 
         //init audio card
         audioFileNameTextView = findViewById(R.id.audio_file_name_textView);
         mixAudioPlayImageButton = findViewById(R.id.mix_audio_play_imageButton);
         mixAudioStopImageButton = findViewById(R.id.mix_audio_stop_imageButton);
+        mixAudioPauseImageButton = findViewById(R.id.mix_audio_pause_imageButton);
         mixAudioRangeStartTextView = findViewById(R.id.mix_audio_range_start_textView);
         mixAudioRangeFinishTextView = findViewById(R.id.mix_audio_range_finish_textView);
         mixAudioRangeSeekBar = findViewById(R.id.mix_audio_range_seekBar);
+        mixAudioSeekBar = findViewById(R.id.mix_audio_seekBar);
         loadAudioFab = findViewById(R.id.load_audio_fab);
 
         //init stat button
@@ -76,20 +97,74 @@ public class Mix extends AppCompatActivity {
         loadVideoFab.setOnClickListener(v -> {
             //load video view
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent,Constants.VIDEO_PICK_REQUEST_CODE);
+            startActivityForResult(intent, Constants.VIDEO_PICK_REQUEST_CODE);
         });
 
         // video view controlling
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                mixVideoSeekBar.setMax(mixVideoView.getDuration());
+                mixVideoSeekBar.setProgress(mixVideoView.getCurrentPosition());
+                handler.postDelayed(this, 50);
+            }
+        };
+
         mixVideoPlayImageButton.setOnClickListener(v -> {
-            if (mixVideoView.isPlaying()) {
-                mixVideoPlayImageButton.setImageResource(R.drawable.pause_icon);
-                mixVideoView.pause();
-            } else {
-                mixVideoPlayImageButton.setImageResource(R.drawable.play_icon);
-                mixVideoView.resume();
+            //handler.removeCallbacks(runnable);
+            if (!mixVideoView.isPlaying()) {
+                if (mixVideoView.getCurrentPosition() == 0)
+                    mixVideoView.start();
+                else {
+                    mixVideoView.start();
+                    mixVideoView.seekTo(mixVideoView.getCurrentPosition());
+                }
+                handler.postDelayed(runnable,0);
             }
         });
-        mixVideoStopImageButton.setOnClickListener(v -> mixVideoView.stopPlayback());
+        mixVideoPauseImageButton.setOnClickListener(v -> {
+            if (mixVideoView.isPlaying()) {
+                mixVideoView.pause();
+                mixVideoSeekBar.setProgress(mixVideoView.getCurrentPosition());
+                handler.removeCallbacks(runnable);
+            }
+            //handler.postDelayed(runnable,50);
+        });
+        mixVideoStopImageButton.setOnClickListener(v -> {
+            mixVideoView.seekTo(0);
+            mixVideoSeekBar.setProgress(0);
+            mixVideoView.pause();
+            handler.removeCallbacks(runnable);
+
+//            handler.post(runnable);
+        });
+
+        mixVideoSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mixVideoView.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+
+        mixVideoRangeSeekBar.setOnRangeSeekbarChangeListener((minValue, maxValue) -> {
+            mixVideoRangeSeekBar.setMinValue(0);
+            mixVideoRangeSeekBar.setMaxValue(mixVideoView.getDuration());
+            Log.i(TAG, "min: " + minValue);
+            Log.i(TAG, "max: " + maxValue);
+            //DecimalFormat df = new DecimalFormat("00.00");
+            mixVideoRangeStartTextView.setText(milliSecondsToTime(minValue.longValue()));
+            mixVideoRangeFinishTextView.setText(milliSecondsToTime(maxValue.longValue()));
+        });
     }
 
     @Override
@@ -98,9 +173,41 @@ public class Mix extends AppCompatActivity {
         if (resultCode == RESULT_OK && requestCode == Constants.VIDEO_PICK_REQUEST_CODE) {
             try {
                 mixVideoView.setVideoURI(data.getData());
-            }catch (NullPointerException e){
-                Log.e(TAG, "onActivityResult: null pointer exception");
+            } catch (Exception e) {
+                Log.e(TAG, "onActivityResult: " + e);
             }
         }
+    }
+
+    private String milliSecondsToTime(long milliseconds) {
+        String finalTimerString = "";
+        String minuteString = "";
+        String secondsString = "";
+
+        // Convert total duration into time
+//            int hours = (int) (milliseconds / (1000 * 60 * 60));
+        int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
+        int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+        // Add hours if there
+//            if (hours > 0) {
+//                finalTimerString = hours + ":";
+//            }
+
+        // Prepending 0 to seconds if it is one digit
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = "" + seconds;
+        }
+        if (minutes < 10) {
+            minuteString = "0" + minutes;
+        } else {
+            minuteString = "" + minuteString;
+        }
+
+        finalTimerString = minuteString + ":" + secondsString;
+
+        // return timer string
+        return finalTimerString;
     }
 }
--- app/src/main/java/com/example/dubsmashmixer/util/Constants.java
+++ app/src/main/java/com/example/dubsmashmixer/util/Constants.java
@@ -2,4 +2,5 @@ package com.example.dubsmashmixer.util;
 
 public class Constants {
     public static int VIDEO_PICK_REQUEST_CODE = 1;
+    public static int READ_EXTERNAL_PERMISSION_REQUEST_CODE = 2;
 }
