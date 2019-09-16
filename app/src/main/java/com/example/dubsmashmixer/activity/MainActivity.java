143bcffe9faaadcd765937476b0ab418c3e9a05a
Festive <27864684+mash-reza@users.noreply.github.com>
2019-09-16 10:30:46 +0430

    read external permission request

--- app/src/main/java/com/example/dubsmashmixer/activity/MainActivity.java
+++ app/src/main/java/com/example/dubsmashmixer/activity/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.dubsmashmixer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.dubsmashmixer.R;
+import com.example.dubsmashmixer.util.Constants;
+import com.google.android.material.canvas.CanvasCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         dub = findViewById(R.id.dubbing);
         dubSmash.setOnClickListener(v -> startActivity(new Intent(this, DubSmash.class)));
         mix.setOnClickListener(v -> startActivity(new Intent(this, Mix.class)));
-        dub.setOnClickListener(v -> startActivity(new Intent(this,Dub.class)));
+        dub.setOnClickListener(v -> startActivity(new Intent(this, Dub.class)));
+        checkPermission();
+    }
+
+    //check permission
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23 &&
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    Constants.READ_EXTERNAL_PERMISSION_REQUEST_CODE);
+        }
     }
 }
